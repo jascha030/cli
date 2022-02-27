c7aab2422c9c11141e199f16b3a10c5aff2b6f0e
Jascha030 <contact@jaschavanaalst.nl>
2022-02-27 19:43:02 +0100

    ✅ Add unittests

---
+++ tests/Shell/ShellTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jascha030\CLI\Tests\Shell;
+
+use Jascha030\CLI\Shell\Shell;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class ShellTest extends TestCase
+{
+    public function testRun(): void
+    {
+        $this->assertEquals('test', (new Shell())->run('echo "test"'));
+    }
+
+    public function testThrowsExceptionOnNonExistingCommand(): void
+    {
+        $this->expectException(ProcessFailedException::class);
+
+        (new Shell())->quietly('somethingthatbreaks');
+    }
+}
