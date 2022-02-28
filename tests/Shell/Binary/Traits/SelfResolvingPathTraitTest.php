eb7c9b65a049b6f323d2775d88b10c922b91f0f0
Jascha030 <contact@jaschavanaalst.nl>
2022-02-28 19:10:37 +0100

    🧪 Add unit tests for SelfResolvingPathTrait.

--- tests/Shell/Binary/Traits/SelfResolvingPathTraitTest.php
+++ tests/Shell/Binary/Traits/SelfResolvingPathTraitTest.php
@@ -1,11 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\CLI\Tests\Shell\Binary\Traits;
 
 use Jascha030\CLI\Shell\Binary\Traits\SelfResolvingPathTrait;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\PhpExecutableFinder;
 
-class SelfResolvingPathTraitTest extends TestCase
+/**
+ * @internal
+ * @covers \Jascha030\CLI\Shell\Binary\Traits\SelfResolvingPathTrait
+ */
+final class SelfResolvingPathTraitTest extends TestCase
 {
+    public function testGetName(): void
+    {
+        $this->assertEquals('php', $this->getTestMock()->getName());
+    }
 
+    public function testGetPath(): void
+    {
+        $path = $this->getTestMock()->getPath();
+
+        // Get target path, if path is a symlink.
+        if (is_link($path)) {
+            $path = realpath($path);
+        }
+
+        $this->assertEquals((new PhpExecutableFinder())->find(), $path);
+    }
+
+    /**
+     * @noinspection PhpTraitUsageOutsideUseInspection
+     */
+    private function getTestMock(): MockObject|SelfResolvingPathTrait
+    {
+        $mock = $this->getMockForTrait(SelfResolvingPathTrait::class);
+
+        $mock->expects($this->any())
+            ->method('getName')
+            ->willReturn('php');
+
+        return $mock;
+    }
 }
